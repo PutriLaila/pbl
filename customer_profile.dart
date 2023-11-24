7ec660bbb81ca64d81f7948cbf96ebaf8f015c0c
PutriLaila <144460632+PutriLaila@users.noreply.github.com>
2023-11-24 13:29:41 +0700

    Create customer_profile.dart

---
+++ customer_profile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ProfileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage(
+                  'assets/logo.png'), // Replace with your profile image
+            ),
+            SizedBox(height: 20),
+            Text(
+              'John Doe', // Replace with your name
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'john.doe@example.com', // Replace with your email
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '+1 123-456-7890', // Replace with your phone number
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+//original name profile_page.dart
